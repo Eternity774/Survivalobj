b11c03f601ab60b43d653d3628af9929b628aa42
Verem4uk <seregaverem4uk@gmail.com>
2018-01-28 21:14:29 +0200

    Restructuring folders + running animation

--- Assets/ForCamera.cs
+++ Assets/ForCamera.cs
@@ -2,19 +2,26 @@
 using System.Collections;
 public class ForCamera : MonoBehaviour
 {
-    
-    private Transform target;
-    public float rotSpeed = 1.5f;
-    private float _rotY = 0;
-    private float _rotX = 0;
+    [SerializeField]
+    private Transform target; 
 
+    public float rotSpeed = 1.5f;
+    private float _rotY;
+    private float _rotX;
+    private Vector3 _offset;
+    void Start()
+    {
+        _rotY = transform.eulerAngles.y;
+        _rotX = transform.eulerAngles.x;
+        _offset = target.position - transform.position; 
+ }
     void LateUpdate()
-    {   
-        _rotY += Input.GetAxis("Mouse Y") * rotSpeed * 3;
-        _rotX += Input.GetAxis("Mouse X") * rotSpeed * 3;
-        Quaternion rotation = Quaternion.Euler(-_rotY, _rotX, 0);
-        transform.localRotation = rotation;
-       // transform.RotateAround(transform.parent, new Vector3)
-      
+    {
+        
+        _rotY += Input.GetAxis("Mouse X") * rotSpeed * 3;
+        _rotX += Input.GetAxis("Mouse Y") * rotSpeed * 3;
+        Quaternion rotation = Quaternion.Euler(_rotX, _rotY, 0);
+        transform.position = target.position - (rotation * _offset); 
+ transform.LookAt(target.transform.position + new Vector3(0,1.5f,0)); 
  }
 }
