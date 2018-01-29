3140ff5cc796270a429ae208b8d0df0db871edfc
Verem4uk <seregaverem4uk@gmail.com>
2018-01-29 22:45:09 +0200

    "smart" rabbit

--- Assets/Player/PlayerMove.cs
+++ Assets/Player/PlayerMove.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
-    float speedx = 1f;
+    float speedx = 3f;
     float speedz = 0.1f;
     Animator animator;
     CharacterController controller;
