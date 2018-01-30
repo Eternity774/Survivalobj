c0d4e3bc511903eaad7917e56cbcfcbf3a251a57
Verem4uk <seregaverem4uk@gmail.com>
2018-01-30 10:27:48 +0200

    Creator of rabbit

---
+++ Assets/Creator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creator : MonoBehaviour {
+    public GameObject rabbitpref;
+	// Use this for initialization
+	void Start () {
+        Instantiate(rabbitpref, new Vector3(Random.Range(-45, 45), 0, Random.Range(-45, 45)), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
