933850e2a804ac474f048bee0b4a75b2408919e9
Verem4uk <seregaverem4uk@gmail.com>
2018-01-31 12:08:20 +0200

    Attack animation of player

--- Assets/Player/PlayerMove.cs
+++ Assets/Player/PlayerMove.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
-    float speedx = 3f;
-    float speedz = 0.1f;
-    Animator animator;
-    CharacterController controller;
+    float speedx = 3f;//скорость поворота 
+    float speedz = 0.1f;//скорость ходьбы
+    Animator animator;//анимаотор
+    CharacterController controller;//контроллер для ходьбы
+    
 	// Use this for initialization
 	void Start () {
         animator = GetComponentInChildren<Animator>();
@@ -15,13 +16,14 @@ public class PlayerMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");//перемещение курсора по горизонтали
+        float z = Input.GetAxis("Vertical");//перемещение курсора по вертикали
 
         if (x != 0)
         {
-            transform.Rotate(0f, x * speedx, 0f);
+            transform.Rotate(0f, x * speedx, 0f);//вращаем персонажа
         }
 
         if (z != 0)
@@ -29,13 +31,13 @@ public class PlayerMove : MonoBehaviour {
             Vector3 dir;
             //if (Input.GetKey(KeyCode.W)) dir = transform.TransformDirection(new Vector3(0f, 0f, z * speedmove));
             // else dir = transform.TransformDirection(new Vector3(0f, 0f, z * speedmove / 2));
-            dir = transform.TransformDirection(new Vector3(0f, 0f, z * speedz));
-            controller.Move(dir);
-            animator.SetBool("Walk", true);
+            dir = transform.TransformDirection(new Vector3(0f, -3f, z * speedz));//высчитываем смещение вперед
+            controller.Move(dir);//двигаем контроллер
+            animator.SetBool("Walk", true);//включаем анимацию ходьбы
         }
-        else
+        else//если не нажата клавиша ходьбы
         {
-           animator.SetBool("Walk", false);
+           animator.SetBool("Walk", false);//выключаем ходьбу
         }
         if(Input.GetKeyDown(KeyCode.LeftShift))
         {
@@ -46,6 +48,13 @@ public class PlayerMove : MonoBehaviour {
         {
             speedx = 1;
             animator.SetBool("Run", false);
+        }
+        
+       if(animator.GetCurrentAnimatorStateInfo(0).IsName("Attack")) animator.SetBool("Attack", false); //проверяем какое сейчас состояние в аниматоре
+        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            animator.SetBool("Attack", true);            
         }
     }
 }
