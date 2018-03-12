841dfb3930656822caed6d698f9c3cc0616cd8a2
Verem4uk <seregaverem4uk@gmail.com>
2018-03-12 18:49:20 +0200

    new after merge

--- Assets/Player/Inventory/Scripts/Item.cs
+++ Assets/Player/Inventory/Scripts/Item.cs
@@ -1,10 +1,87 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class Item{
+public enum itemType
+{
+	consumable,
+	weapon,
+	armor
+}
+
+public interface IStrategy
+{
+	void Algorithm ();
+}
+
+public class iConsumable: IStrategy
+{
+	public void Algorithm()
+	{
+		Debug.Log ("Using consumable!");
+	}
+}
+
+public class iWeapon: IStrategy
+{
+	public void Algorithm()
+	{
+		Debug.Log ("Using weapon!");
+	}
+}
+
+public class iArmor: IStrategy
+{
+	public void Algorithm()
+	{
+		Debug.Log ("Using armor!");
+	}
+}
+
+public class Use{
 	
-	public Texture2D Textura;
-	public string Name;
+	private IStrategy _strategy;
+
+	public Use (IStrategy strategy){
+		_strategy = strategy;
+	}
+
+	public void SetStrategy(IStrategy strategy){
+		_strategy = strategy;
+	}
+
+	public void ExecuteOperation(){
+		_strategy.Algorithm ();
+	}
+}
+
+[CreateAssetMenu(fileName="New Item", menuName="Inventory/Item")]
+public class Item : ScriptableObject {
+
+
+	new public string name = "New Item";
+	public Sprite icon = null;
+	public bool isDefaultItem=false;
+	public itemType current; 
+
+	public virtual void UseItem()
+	{
+		Use use = new Use (new iArmor ());
+				switch(current)
+				{
+					case itemType.armor:
+						use.SetStrategy (new iArmor ());
+						use.ExecuteOperation ();
+						break;
+					case itemType.weapon:
+						use.SetStrategy (new iWeapon ());
+						use.ExecuteOperation ();
+						break;
+					case itemType.consumable:
+						use.SetStrategy (new iConsumable ());
+						use.ExecuteOperation ();
+						break;
+		
+				}
+	}
+
 }
---
+++ Assets/Player/Inventory/Scripts/ItemPickup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour {
+
+	public Item item;
+
+	public void OnTriggerEnter(Collider col){
+		if (col.tag == "Player") {
+			
+			bool wasPickedUp=Inventory.instance.Add(item);
+			if (wasPickedUp) {
+				Destroy (gameObject);
+				//Inventory.instance.Add (item);
+			}
+				Debug.Log("You picked up "+ gameObject.name);
+		}
+	}
+//	void PickUp(){
+//	//Adding item to inventory
+//		bool wasPickedUp=Inventory.instance.Add(item);
+//		if (wasPickedUp) 
+//			Destroy (gameObject);
+//	}
+}
