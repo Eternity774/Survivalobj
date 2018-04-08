ea5b1853d784d6b17dff6c61cfe0a71c40197899
Roman <eternity774@gmail.com>
2018-04-08 11:42:08 +0300

    Added panel for crafting and some models of ingridients, NEW TYPE SCRIPTABLEOBJECTS - Ingridients

--- Assets/Player/Inventory/Scripts/InventoryUI.cs
+++ Assets/Player/Inventory/Scripts/InventoryUI.cs
@@ -3,19 +3,23 @@
 public class InventoryUI : MonoBehaviour {
 	
 	public Transform itemsParent;
+	public Transform craftingParent;
 	//public Transform equipParent;
 
 
 	Inventory inventory;
-
+	Crafting crafting;
 	InventorySlot[] slots;
+	CraftingSlot[] c_slots;
 	//CharacterSlot[] e_slots;
 	// Use this for initialization
 	void Start () {
 		inventory = Inventory.instance;
+		crafting = Crafting.instance;
 		inventory.onItemChangedCallback += UpdateUI;
 	
 		slots = itemsParent.GetComponentsInChildren<InventorySlot> ();
+		c_slots = craftingParent.GetComponentsInChildren<CraftingSlot> ();
 	//	e_slots = equipParent.GetComponentsInChildren<CharacterSlot> ();
 	}
 	
@@ -26,6 +30,13 @@ public class InventoryUI : MonoBehaviour {
 				slots [i].AddItem (inventory.items [i]);
 			} else {
 				slots [i].ClearSlot ();
+			}
+		}
+		for (int i = 0; i < c_slots.Length; i++) {
+			if (i < crafting.crafts.Count) {
+				c_slots [i].AddItem (crafting.crafts [i]);
+			} else {
+				c_slots [i].ClearSlot ();
 			}
 		}
 	}
